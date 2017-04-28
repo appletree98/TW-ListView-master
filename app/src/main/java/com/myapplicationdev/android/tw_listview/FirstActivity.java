1720c9a50664148c4a510affa4034dc0c37bd9a2
appletree98 <15017452@myrp.edu.sg>
2017-04-28 12:52:53 +0800

    new AL entry

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -2,8 +2,11 @@ package com.myapplicationdev.android.tw_listview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,10 +22,13 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = (ListView)findViewById(R.id.listview);
 
+        // adding new data into AL
         al = new ArrayList<String>();
         al.add("Year 1");
-
+        al.add("Year 2");
+        al.add("Year 3");
 
 
 
